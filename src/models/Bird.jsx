573f320198b6c66a1475307dd9b8b1521221f163
Richter Lau <ar.rick@hotmail.com>
2023-11-23 16:45:03 -0500

    updating bird

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,6 +1,6 @@
 import { useGLTF } from '@react-three/drei'
 import React from 'react'
-import birdScene from '../assets/3d/Bird.glb'
+import birdScene from '../assets/3d/bird.glb'
 
 const Bird = () => {
   const { scene, animations } = useGLTF(birdScene)
