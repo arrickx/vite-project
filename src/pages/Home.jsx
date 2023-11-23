794b9a763fdd1502137ef97550b4be310477b8bd
Richter Lau <ar.rick@hotmail.com>
2023-11-23 16:07:58 -0500

    updating lights

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,6 +30,10 @@ const Home = () => {
       <Canvas className='w-full h-screen bg-transparent' camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
           <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
         </Suspense>
       </Canvas>
