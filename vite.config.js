9d690604425cbc3ad1f533c9949bf867ffca0498
Richter Lau <ar.rick@hotmail.com>
2023-11-23 16:05:22 -0500

    setting up island

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
   // unhide for deploy in github page
   base: '/vite-project/',
 });
