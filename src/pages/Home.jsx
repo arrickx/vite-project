c4ec0c924ba6892394d86bda2531a20f64abf5c9
Richter Lau <ar.rick@hotmail.com>
2023-12-04 18:07:58 -0500

    adding home info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import { Canvas } from '@react-three/fiber';
 import { Suspense, useState } from 'react';
+import HomeInfo from '../components/HomeInfo';
 import Loader from '../components/Loader';
 import Bird from '../models/Bird';
 import Island from '../models/Island';
 import Plane from '../models/Plane';
 import Sky from '../models/Sky';
 
-{/* <div className='abosollute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        popup window
-      </div> */}
+
 
 
 const Home = () => {
@@ -50,6 +49,10 @@ const Home = () => {
 
   return (
     <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
+
       <Canvas
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}>
@@ -60,7 +63,7 @@ const Home = () => {
           <Bird />
           <Island position={islandPosition} scale={islandScale} rotation={islandRotation} isRotating={isRotating} setIsRotating={setIsRotating} setCurrentStage={setCurrentStage} />
           <Plane isRotating={isRotating} scale={planeScale} position={planePosition} rotation={[0, 20, 0]} />
-          <Sky isRotating={isRotating}/>
+          <Sky isRotating={isRotating} />
         </Suspense>
       </Canvas>
     </section>
