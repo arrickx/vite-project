5779b08b79856b77c69022c7da3a34df7b8043d2
Richter Lau <ar.rick@hotmail.com>
2023-12-05 19:41:11 -0500

    adding about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+
+import { skills } from '../constants';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm{' '}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          Richter
+        </span>
+      </h1>
 
-export default About
+      <div className='mt-5 flex-col gap-3 text-slate-500'>
+        <p>
+          Software Engineer based in New York, specializing in the technical
+          education through hands-on learning and building applications.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'> My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='py-16'>
+        <h3 className='subheaded-text'>Work Experience</h3>
+        <div className='mt-5 flex-col gap-3 text-slate-500'>
+          <p>
+            I've worked with all sorts of companies, leveling up my skills and
+            teaming up with smart people. Here's the rundown:
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
